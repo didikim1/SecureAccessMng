cfd5c432a93e1d6d9c9542ea75d035cd7dc1bb72
hsjeon1224 <hsjeon1224@inbiznetcorp.com>
2020-08-06 13:51:48 +0900

    .

--- SecureAccessMng-Web/src/main/java/com/inbiznetcorp/SecureAccess/SecureAccessMng/Web/api/act/CommAPIAction.java
+++ SecureAccessMng-Web/src/main/java/com/inbiznetcorp/SecureAccess/SecureAccessMng/Web/api/act/CommAPIAction.java
@@ -1,7 +1,15 @@
 package com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.api.act;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +17,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.framework.beans.FrameworkBeans;
+import com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.framework.mymap.MyMap;
+import com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.framework.utils.FrameworkUtils;
+import com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.httpClient.HttpRequestProc;
 
 @Controller
 @RequestMapping("/comm/api")
@@ -21,7 +34,6 @@ public class CommAPIAction
     @RequestMapping(value ={ "/getPublicIP.do" })
     public @ResponseBody String ListPagingData(Model model, HttpServletRequest request)
     {
-        System.out.println("@@@");
         String ip = request.getHeader("X-FORWARDED-FOR");
 
         if (ip == null || ip.length() == 0)
@@ -38,10 +50,53 @@ public class CommAPIAction
         {
           ip= request.getRemoteAddr() ;
         }
-
-
         return ip;
     }
-
+    
+	@RequestMapping(value ={ "/arsAuth.do" })
+    public void arsAuth(HttpServletRequest request, HttpServletResponse response)
+    {
+		PrintWriter     printWriter                     = null;
+	    
+    	MyMap 		paramMap 		= FrameworkBeans.findHttpServletBean().findClientRequestParameter();
+    	JSONObject jsonObjectResult = null; 
+    	
+    	HashMap<String, String> arsAuthMap = new HashMap<String, String>();
+    	
+    	
+    	arsAuthMap.put("authReqNumber",        	paramMap.getStr("authReqNumber"));
+    	arsAuthMap.put("requestTime",          	paramMap.getStr("requestTime"));
+    	arsAuthMap.put("companyCode",          	paramMap.getStr("companyCode"));
+    	arsAuthMap.put("serviceCode",          	paramMap.getStr("serviceCode"));
+    	arsAuthMap.put("serviceType",          	paramMap.getStr("serviceType"));
+    	arsAuthMap.put("phoneNumber",          	paramMap.getStr("phoneNumber"));
+    	arsAuthMap.put("authNumber",   			paramMap.getStr("authNumber"));
+    	
+    	arsAuthMap.put("INFO1",   				FrameworkUtils.unescapeHtml(paramMap.getStr("INFO1")));   
+    	arsAuthMap.put("INFO2",   				FrameworkUtils.unescapeHtml(paramMap.getStr("INFO2")));   
+    	
+    	String strJSONResult = new HttpRequestProc().sendPacket("https://auth_test.ring2pay.com:7910/authgwSync/asp/arsauth/sync/r2p_auth.do", arsAuthMap);
+    	System.out.println("strJSONResult:::"+strJSONResult);
+    	
+    	try 
+    	{
+			jsonObjectResult = (JSONObject) new JSONParser().parse(strJSONResult);
+		} 
+    	catch (ParseException e) 
+    	{
+			e.printStackTrace();
+		}
+    	
+        try {
+        	response.setContentType("text/html; charset=utf-8");
+			printWriter = response.getWriter();
+			printWriter.print(jsonObjectResult.toString());
+			printWriter.flush();
+			printWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
 
 }
---
+++ SecureAccessMng-Web/src/main/java/com/inbiznetcorp/SecureAccess/SecureAccessMng/Web/httpClient/HttpRequestProc.java
@@ -0,0 +1,90 @@
+package com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.httpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class HttpRequestProc
+{
+
+    public String sendPacket(String url, Map<String, String> paramMap)
+    {
+        List<NameValuePair> paramList           = this.map2NameValuePairList(paramMap);
+
+        return this.excute(url, paramList);
+    }
+
+    @SuppressWarnings("deprecation")
+    private String excute(String url, List<NameValuePair> paramList)
+    {
+        HttpClient client = new DefaultHttpClient();
+        StringBuffer returnData = new StringBuffer();
+        BufferedReader rd = null;
+        int soketTimeout = Integer.valueOf(180);
+        try
+        {
+            client.getParams().setParameter("http.socket.timeout", new Integer(soketTimeout * 1000));
+            client.getParams().setParameter("http.protocol.content-charset", "UTF-8");
+
+            HttpPost post = new HttpPost(url);
+
+            post.setEntity(new UrlEncodedFormEntity(paramList, "UTF-8"));
+
+            HttpResponse response = client.execute(post);
+
+            rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+            String line = "";
+            while ((line = rd.readLine()) != null)
+            {
+                returnData.append(line);
+            }
+            rd.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+
+            return null;
+        }
+        finally
+        {
+            if (rd != null)
+                try
+                {
+                    rd.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            client.getConnectionManager().shutdown();
+        }
+        return returnData.toString();
+    }
+
+    private List<NameValuePair> map2NameValuePairList(Map<String, String> paramMap)
+    {
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        Iterator<String> keys = paramMap.keySet().iterator();
+        while (keys.hasNext())
+        {
+            String key = keys.next();
+            nameValuePairs.add(new BasicNameValuePair(key, paramMap.get(key)));
+        }
+        return nameValuePairs;
+    }
+}
