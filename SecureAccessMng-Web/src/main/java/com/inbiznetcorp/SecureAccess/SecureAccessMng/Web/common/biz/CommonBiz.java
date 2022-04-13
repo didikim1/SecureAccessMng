6f2002b5d17652e654c2da746089d9bc9a1cc5a7
hyosung <amok87@nate.com>
2022-04-13 10:21:46 +0900

    .

--- SecureAccessMng-Web/src/main/java/com/inbiznetcorp/SecureAccess/SecureAccessMng/Web/common/biz/CommonBiz.java
+++ SecureAccessMng-Web/src/main/java/com/inbiznetcorp/SecureAccess/SecureAccessMng/Web/common/biz/CommonBiz.java
@@ -1,11 +1,36 @@
 package com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.common.biz;
 
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.framework.utils.FrameworkUtils;
+import com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.httpClient.RequestProcJSON;
 
 @Service("com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.common.biz.CommonBiz")
 public class CommonBiz
 {
     @Value("${spring.profiles.active}")
     public String active;
+
+
+    public JSONObject authCallSender(String phoneNumber, String authNumber)
+    {
+        JSONObject          strJSONObject = new JSONObject();
+
+        // 인증
+        strJSONObject.put("authReqNumber",        "TEST_"+FrameworkUtils.generateSessionID());
+        strJSONObject.put("requestTime",          FrameworkUtils.getDateToStr());
+        strJSONObject.put("companyCode",          "43418");
+        strJSONObject.put("serviceCode",          "0001");
+        strJSONObject.put("serviceType",          "01");
+        strJSONObject.put("phoneNumber",          phoneNumber); // 전화번호
+        strJSONObject.put("authNumber",           authNumber); // 전화번호
+
+        JSONObject jsonObject = new RequestProcJSON().sendPacket("https://auth_test.ring2pay.com:7891/asp/arsauth/sync/rest/r2p_paauth.do", strJSONObject.toString());
+
+        System.out.println("jsonObject : " + jsonObject);
+
+        return jsonObject;
+    }
 }
--- SecureAccessMng-Web/src/main/java/com/inbiznetcorp/SecureAccess/SecureAccessMng/Web/ctn/nrlmber/act/NrlmberAct.java
+++ SecureAccessMng-Web/src/main/java/com/inbiznetcorp/SecureAccess/SecureAccessMng/Web/ctn/nrlmber/act/NrlmberAct.java
@@ -9,12 +9,14 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.common.biz.CommonBiz;
 import com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.ctn.charge.biz.ChargeBiz;
 import com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.ctn.nrlmber.biz.NrlmberBiz;
 import com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.ctn.role.biz.RoleBiz;
@@ -39,18 +41,21 @@ public class NrlmberAct
 
 	@Resource(name="com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.ctn.nrlmber.biz.NrlmberBiz")
 	NrlmberBiz mBiz;
-	
+
 	@Resource(name="com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.ctn.role.biz.RoleBiz")
 	RoleBiz mRoleBiz;
-	
+
 	@Resource(name="com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.ctn.charge.biz.ChargeBiz")
 	ChargeBiz mChargeBiz;
 
 	@Resource(name = "com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.framework.excel.ExcelWrite")
-    ExcelWrite mExcelWrite;
-	
+        ExcelWrite mExcelWrite;
+
 	 @Resource(name = "com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.ctn.nrlmber.biz.NrlmberBiz")
 	 NrlmberBiz mNrlmberBiz;
+
+	 @Resource(name="com.inbiznetcorp.SecureAccess.SecureAccessMng.Web.common.biz.CommonBiz")
+	 CommonBiz mCommonBiz;
 
 	@RequestMapping(value = { "/ListPagingData.do" })
 	public String ListPagingDatas(Model model)
@@ -105,19 +110,19 @@ public class NrlmberAct
 	{
 		MyMap paramMap        = FrameworkBeans.findHttpServletBean().findClientRequestParameter();
 		MyMap resultMap       = null;
-		
+
 		BasicBean roleList	  = null;
 		BasicBean chargeList  = null;
-		
+
 		resultMap	 = mBiz.SelectOneData(paramMap);
 		roleList  	 = mRoleBiz.ListPagingData(new MyMap());
 		chargeList	 = mChargeBiz.ListPagingData(new MyMap());
-		
+
 		 model.addAttribute("paramMap",      paramMap);
 		 model.addAttribute("Info",          resultMap);
 		 model.addAttribute("RoleList",      roleList.getList());
 		 model.addAttribute("ChargeList",    chargeList.getList());
-		
+
 		return pagePrefix + "/RegisterData";
 	}
 
@@ -126,14 +131,14 @@ public class NrlmberAct
 	{
 		MyMap paramMap        = FrameworkBeans.findHttpServletBean().findClientRequestParameter();
 		MyMap resultMap       = null;
-		
+
 		BasicBean roleList	  = null;
 		BasicBean chargeList  = null;
-		
+
 		resultMap  = mBiz.SelectOneData(paramMap);
 		roleList   = mRoleBiz.ListPagingData(new MyMap());
 		chargeList = mChargeBiz.ListPagingData(new MyMap());
-		
+
 		model.addAttribute("paramMap",      paramMap);
 		model.addAttribute("Info",          resultMap);
 		model.addAttribute("RoleList",      roleList.getList());
@@ -147,15 +152,15 @@ public class NrlmberAct
 	{
 	    MyMap              paramMap                        = FrameworkBeans.findHttpServletBean().findClientRequestParameter();
 	    MyCamelMap         resultMap                       = new MyCamelMap();
-	    
+
 	    String             resultCode                      = ResultCode.RESULT_INTERNAL_SERVER_ERROR;
 	    int                resultRegisterDataCount         = 0;
 
-	    	
+
         if(paramMap.getInt("seq", 0) > 0 )
         {
                 resultCode      = ResultCode.RESULT_OK;
-                resultRegisterDataCount = mBiz.ModifyData(paramMap);            
+                resultRegisterDataCount = mBiz.ModifyData(paramMap);
         }
         else
         {
@@ -168,7 +173,7 @@ public class NrlmberAct
 
 	    return new ResultMessage(resultCode, null);
 	}
-	
+
 
 	@RequestMapping(value = { "/ModifyData.do" })
 	public String ModifyData(Model model)
@@ -179,7 +184,7 @@ public class NrlmberAct
 
 		return pagePrefix + "/ModifyData";
 	}
-	
+
 	@RequestMapping(value = { "/ProcModifyData.do" })
 	public ResultMessage ProcModifyData(Model model)
 	{
@@ -198,16 +203,16 @@ public class NrlmberAct
 		MyMap 	paramMap 				  	= FrameworkBeans.findHttpServletBean().findClientRequestParameter();
 		String 	resultCode			  		= ResultCode.RESULT_INTERNAL_SERVER_ERROR;
 		int   	resultRegisterDataCount 	= 0;
-		
+
 		resultRegisterDataCount = mBiz.DeleteData( paramMap );
-		
+
 		if( resultRegisterDataCount > 0)
 		{
 			resultCode			  		= ResultCode.RESULT_OK;
 		}
-		
+
 		 model.addAttribute("paramMap",      paramMap);
-		 
+
 		 return new ResultMessage(ResultCode.RESULT_OK, null);
 	}
 
@@ -228,29 +233,29 @@ public class NrlmberAct
 
             return new ResultMessage(resultCode, resultMap);
 	}
-		
+
 	// 엑셀 다운로드
 	@RequestMapping(value = { "/ListExcelData.do" })
 	public void ListExcelData(HttpServletRequest request, HttpServletResponse response, Model model)throws WriteException, IOException
 	{
 		List<MyCamelMap> resultS03Excel    = null;
         MyMap            paramMap          = FrameworkBeans.findHttpServletBean().findClientRequestParameter();
-		
+
         BasicBean       resultBean  		= null;
         String      strFileName      		= "계정상세자료 ("+paramMap.getStr("sDate")+"~"+paramMap.getStr("eDate")+").xlsx";
 		String [] arrTitle           			= new String[]{"처리자", 			"소유자",					"계정",				"전화번호",				"담당",				"권한",			"담당책임",			"상태"};
 		String [] arrExcelColum         = new String[] {"uniqId",		"mberName",	"emailAddress",	"moblphonNo",		"chargeId",	"roleId",		"mberRating",	"mberSttus"};
-		
+
 		paramMap.put("rows",1000000);
 		resultBean = mBiz.ListPagingData( paramMap );
-		
+
 		// 엑셀 데이터 나오는거 때문에 그러는거지?네네
 		// public void selectExcelList(HttpServletResponse response, String[] title, String[] cols, List<MyCamelMap> list, String fileName) {
 		// mExcelWrite 이 클래스에서 selectExcelList 이것만 사용하면될것같아
 		// tcp 안에 http가 있는거고 tcp 소켓은 인아웃에 스트림을 있찌
 		// HttpServletRequest 요청스트림
 		// HttpServletResponse 응답스트림
-		// 응답스트림에 엑셀을 내려주는건데 
+		// 응답스트림에 엑셀을 내려주는건데
 		// selectExcelList 오면 `list` 리스트만 실어주면될듯해
 		//
 		// BasicBean       resultBean  		= null;
@@ -260,18 +265,18 @@ public class NrlmberAct
 		// 여긴 mExcelWrite 함수한테 `response` 응답 스트림을 넘겨줌
 		// 근데 엑셀은 10개만 할게 아니지?ㅎㅎ
 		// 잘바바
-		// 11 rows가 엑셀에 나왔고 
-		// 엑셀다운로들 누를때 limit 0 ~ 1000000 까지 
+		// 11 rows가 엑셀에 나왔고
+		// 엑셀다운로들 누를때 limit 0 ~ 1000000 까지
 		// 그냥 페이지은 걍 10 씩만 나오는거고..ㅎ
 		// ㅇㅋㅇㅋㅋㅇㅋ???네!!!
 //		String [] arrvalue         			= new String[] {"${info.uniqId}",		"mberName",	"emailAddress",	"moblphonNo",		"chargeId",	"roleId",		"mberRating",	"mberSttus"};
-		
+
 //		resultS03Excel = mExcelWrite.ListData( paramMap );
-		
+
 		 mExcelWrite.selectExcelList(response, arrTitle, arrExcelColum, resultBean.getList(),  strFileName);
-		
+
 	}
-	
+
     @RequestMapping(value ={ "/ListData.do" })
     public @ResponseBody ResultMessage ListData(Model model)
     {
@@ -285,4 +290,45 @@ public class NrlmberAct
 
         return new ResultMessage(ResultCode.RESULT_OK, mNrlmberBiz.ListPagingData(nrlmListParamMap) );
     }
+    /*
+     * 수정화면에서 [수정] 버튼을 클릭하고
+     * CallAuthPage.do >
+     *
+     *
+     */
+    @RequestMapping(value = { "/CallAuthPage.do" })
+    public String CallAuthPage(Model model)
+    {
+            MyMap paramMap = FrameworkBeans.findHttpServletBean().findClientRequestParameter();
+
+            // 랜덤 숫자 두자리를 생성 전달 `https://offbyone.tistory.com/237`
+//            String phoneNumber = paramMap.getStr("phoneNumber");  //  해당 컨트롤러
+            String authNumber  = null;
+
+            /*랜덤숫자 두자리 생성*/
+            // authNumber = fsfsfs
+
+             model.addAttribute("paramMap",         paramMap);
+//             model.addAttribute("phoneNumber",      phoneNumber);
+             model.addAttribute("authNumber",       authNumber);
+
+            return pagePrefix + "/CallAuthPage";
+    }
+
+    // 전화하는 컨트롤러
+    @RequestMapping(value = { "/CallAuth.do" })
+    public @ResponseBody ResultMessage  CallAuth(Model model)
+    {
+            MyMap paramMap = FrameworkBeans.findHttpServletBean().findClientRequestParameter();
+
+             // 
+
+
+             model.addAttribute("paramMap",      paramMap);
+
+             JSONObject rtrn =  mCommonBiz.authCallSender(paramMap.getStr("phoneNumber"), paramMap.getStr("authNumber"));
+
+             return new ResultMessage(ResultCode.RESULT_OK, rtrn );
+    }
+
 }
